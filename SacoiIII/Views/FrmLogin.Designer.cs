759af1c462491c0d5e587fab49c967e9145943fc
Matt-Alves07 <mathalves07.winstore@outlook.com>
2018-05-31 17:14:33 -0300

    Criação do formulário de login e do botão sair. Adicionado novo icone padrão e novo icone para o botão sair da tela de login

---
+++ SacoiIII/Views/FrmLogin.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SacoiIII
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.PnlMenu = new System.Windows.Forms.Panel();
+            this.PnlButtons = new System.Windows.Forms.Panel();
+            this.Hint = new System.Windows.Forms.ToolTip(this.components);
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.PnlMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlMenu
+            // 
+            this.PnlMenu.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.PnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(102)))), ((int)(((byte)(255)))));
+            this.PnlMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PnlMenu.Controls.Add(this.BtnSair);
+            this.PnlMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.PnlMenu.Name = "PnlMenu";
+            this.PnlMenu.Size = new System.Drawing.Size(313, 41);
+            this.PnlMenu.TabIndex = 0;
+            // 
+            // PnlButtons
+            // 
+            this.PnlButtons.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.PnlButtons.BackColor = System.Drawing.Color.Transparent;
+            this.PnlButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PnlButtons.Location = new System.Drawing.Point(0, 269);
+            this.PnlButtons.Name = "PnlButtons";
+            this.PnlButtons.Size = new System.Drawing.Size(313, 82);
+            this.PnlButtons.TabIndex = 1;
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSair.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnSair.FlatAppearance.BorderSize = 0;
+            this.BtnSair.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.BtnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSair.ForeColor = System.Drawing.Color.White;
+            this.BtnSair.Image = global::SacoiIII.Properties.Resources.power;
+            this.BtnSair.Location = new System.Drawing.Point(247, 0);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(64, 39);
+            this.BtnSair.TabIndex = 0;
+            this.BtnSair.TabStop = false;
+            this.BtnSair.Text = " &Sair";
+            this.BtnSair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Hint.SetToolTip(this.BtnSair, "Clique aqui para sair");
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 351);
+            this.Controls.Add(this.PnlButtons);
+            this.Controls.Add(this.PnlMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SACOI - Login";
+            this.PnlMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlMenu;
+        private System.Windows.Forms.Panel PnlButtons;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.ToolTip Hint;
+    }
+}
+
---
+++ SacoiIII/Views/FrmLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SacoiIII
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realemte sair?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
