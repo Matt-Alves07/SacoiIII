46b40319593f8fd59b3db21af147b18955f9a35f
Joao <jdcs-tkd@live.com>
2018-06-08 14:27:51 -0300

    Sistema de email corrigido e testado

--- SacoiIII/Views/FrmMessageA.cs
+++ SacoiIII/Views/FrmMessageA.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 using System.Net.Mail;
 
 namespace SacoiIII.Views
@@ -27,27 +28,45 @@ namespace SacoiIII.Views
         {
             try
             {
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient("smtp-mail.outlook.com");
 
-                mail.From = new MailAddress("sacoi.tcc@outlook.com");
-                mail.To.Add(TxtTo.Text);
-                mail.Subject = TxtSubject.Text;
-                mail.Body = TxtBody.Text;
+                //Cria um client SMTP para enviar o email. (host, porta)
+                SmtpClient client = new SmtpClient("smtp-mail.outlook.com", 587);
 
-                SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("sacoi.tcc@outlook.com", "M1nh@S3nh@");
-                SmtpServer.EnableSsl = true;
+                //Aqui informa ao programa qual email (junto com a senha) que irá enviar.
+                NetworkCredential cred = new NetworkCredential("sacoi.tcc@outlook.com.br", "m1nh4s3nh4");
 
-                SmtpServer.Send(mail);
-                MessageBox.Show("Email enviado com sucesso!", "Email Enviado.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //Código para criar o email (sem as informações) que iremos mandar.
+                MailMessage msg = new MailMessage();
+
+                //Enviando informações do email
+                //Email remetente
+                msg.From = new MailAddress("sacoi.tcc@outlook.com.br");
+
+                //Email destinatário
+                msg.To.Add(TxtTo.Text);
+
+                //Assunto do email
+                msg.Subject = TxtSubject.Text;
+
+                //Corpo de texto do email
+                msg.Body = TxtBody.Text;
+
+                //Envia para o SACOI as credenciais do email
+                client.Credentials = cred;
+
+                //Habilita o SSL (Secure Sockets Layer) que os servidores de email pedem.
+                client.EnableSsl = true;
+
+                //Envia o email
+                client.Send(msg);
+
+                MessageBox.Show("Email enviado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            catch (Exception ex)
+            catch
             {
-                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Não foi possível enviar o email", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             TxtBody.Text = "";
